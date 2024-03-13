7ca78cab3c639a63a03b25ca8800a4b55c261344
tagiyevis <116768623+tagiyevis@users.noreply.github.com>
2024-03-14 01:56:38 +0400

    omnitech

---
+++ swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
